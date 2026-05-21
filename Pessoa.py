7e2075f48dde84a89b203e29173fa8f82f4b5340
alifrebat <alifrebat@gmail.com>
2026-05-21 14:33:24 -0300

    Criado a classe Pessoa

---
+++ Pessoa.py
@@ -0,0 +1,21 @@
+"""
+    Documentar classe é descrição da classe
+    Dizer que ela faz
+    Classe Pojo: É uma classe que contem atributos e os métodos setts e getts
+"""
+class Pessoa:
+    def __init__(self, objPessoa):
+        self.nome = objPessoa["nome"]
+        self.idade = objPessoa["idade"]
+        self.renda = objPessoa["renda"]
+
+
+
+   
+    
+
+
+
+
+
+
